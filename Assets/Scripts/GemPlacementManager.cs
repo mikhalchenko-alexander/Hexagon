0acf149318a83c34f99b19896888467136a6cbab
aam <aam@anahoret.com>
2018-09-07 18:12:10 +0300

    Add gem placement manager.

---
+++ Assets/Scripts/GemPlacementManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GemPlacementManager : MonoBehaviour {
+	
+	[SerializeField] private Tilemap _tilemap;
+
+	public void PutGem(Gem gem, Vector3Int axialCoordinates) {
+		var offsetCoords = CoordinateUtils.AxialToOffset(axialCoordinates);
+		gem.transform.position = _tilemap.GetCellCenterWorld(offsetCoords);
+	}
+	
+}
