188fa4b17f0109e859c660444401eba5fbf62fc6
aam <aam@anahoret.com>
2018-09-07 18:10:24 +0300

    Fix coordinate conversion.

--- Assets/Scripts/CoordinateUtils.cs
+++ Assets/Scripts/CoordinateUtils.cs
@@ -3,17 +3,17 @@
 public class CoordinateUtils {
 
 	public static Vector3Int OffsetToAxial(Vector3Int offset) {
-		var x = offset.x;
-		var z = offset.y - (offset.x - (offset.x & 1)) / 2;
-		var y = -x - z;
+		var x = offset.y;
+		var z = -offset.x - (offset.y + (offset.y & 1)) / 2;
+		var y = -offset.y - z;
 		
 		return new Vector3Int(x, y, z);
 	}
 
 	public static Vector3Int AxialToOffset(Vector3Int axial) {
-		var x = axial.x;
-		var y = axial.z + (axial.x - (axial.x & 1)) / 2;
-		return new Vector3Int(x, y, 0);
+		var y = axial.x;
+		var x = axial.z + (axial.x + (axial.x & 1)) / 2;
+		return new Vector3Int(-x, y, 0);
 	}
 	
 }
