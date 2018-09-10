840040b8b203d1b34d05c5c2cb324781635a6b67
aam <aam@anahoret.com>
2018-09-10 17:37:36 +0300

    Add alternative rectangle board.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     public void StartGame() {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("RectangleBoard");
     }
     public void QuitGame() {
         Application.Quit();
