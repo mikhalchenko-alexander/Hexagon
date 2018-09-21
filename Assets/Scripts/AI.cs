48f3dfcd26d500d2761abd8b0fbe4095a44cb521
aam <aam@anahoret.com>
2018-09-21 14:50:17 +0300

    Add AI architecture.

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -16,7 +16,7 @@ public struct AiMove {
     }
 }
 
-public struct Board {
+public class Board {
     public readonly List<Vector3Int> Grid;
     public readonly List<Vector3Int> RedGems;
     public readonly List<Vector3Int> BlueGems;
@@ -26,32 +26,44 @@ public struct Board {
         RedGems = redGems;
         BlueGems = blueGems;
     }
+    
+    public List<Vector3Int> GetEmptyNeighbours(Vector3Int cell) {
+        var neighbours = CoordinateUtils.Neighbours(cell)
+            .Where(Grid.Contains)
+            .ToList();
+
+        var jumpNeighbours = neighbours.SelectMany(CoordinateUtils.Neighbours)
+            .Where(Grid.Contains)
+            .Where(n => n != cell && !neighbours.Contains(n));
+
+        var emptyNeighbours = neighbours.Concat(jumpNeighbours)
+            .Where(n => !RedGems.Contains(n) && !BlueGems.Contains(n))
+            .ToList();
+            
+        return emptyNeighbours;
+    }
+
 }
 
 public static class Ai {
 
     public static AiMove GetMove(Board board) {
-        var firstMovableGem = board.BlueGems.First(gem => GetEmptyNeighbours(gem, board).Count > 0);
-
-        var firstAvailableMove = GetEmptyNeighbours(firstMovableGem, board)[0];
-        
-        return new AiMove(firstMovableGem, firstAvailableMove);
+        return BestMove(board);
     }
 
-    private static List<Vector3Int> GetEmptyNeighbours(Vector3Int cell, Board board) {
-        var neighbours = CoordinateUtils.Neighbours(cell)
-            .Where(board.Grid.Contains)
-            .ToList();
-
-        var jumpNeighbours = neighbours.SelectMany(CoordinateUtils.Neighbours)
-            .Where(board.Grid.Contains)
-            .Where(n => n != cell && !neighbours.Contains(n));
-
-        var emptyNeighbours = neighbours.Concat(jumpNeighbours)
-            .Where(n => !board.RedGems.Contains(n) && !board.BlueGems.Contains(n))
-            .ToList();
-            
-        return emptyNeighbours;
+    private static AiMove BestMove(Board board) {
+        var allMoves = GetAllMoves(board);
+        var estimatedMoves = Estimate(allMoves);
+        return estimatedMoves.OrderBy(kv => kv.Value).First().Key;
     }
-    
+
+    private static List<KeyValuePair<AiMove, int>> Estimate(List<AiMove> moves) {
+        return moves.Select(move => new KeyValuePair<AiMove, int>(move, 0)).ToList();
+    }
+
+    private static List<AiMove> GetAllMoves(Board board) {
+        return board.BlueGems
+            .SelectMany(from => board.GetEmptyNeighbours(from).Select(to => new AiMove(from, to)))
+            .ToList();
+    }
 }
